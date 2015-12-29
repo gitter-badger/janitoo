26487e678478e071c9db79e4ab22366a3fb3c649
bibi21000 <bibi21000@gmail.com>
2015-12-29 01:07:28 +0100

    Update setup.py to embed data files

--- docker/auto.sh
+++ docker/auto.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
 cd /root/glances
-git pull origin develop
+#git pull origin develop
 #You must loop here
-python -m glances
+nice -n 19 python -m glances
