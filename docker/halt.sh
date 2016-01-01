27e533efff4f9955d624db0d3c8628e2b07a39b1
bibi21000 <bibi21000@gmail.com>
2016-01-01 02:57:21 +0100

    Update halt script

--- docker/halt.sh
+++ docker/halt.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 echo "Halting ..."
 killall supervisord
-echo "Sleeping 5 seconds ..."
+echo "Waiting for shutdown for 10 seconds ..."
+sleep 10
+ps aux
+echo "Waiting for shutdown for 10 seconds ..."
+sleep 10
+ps aux
+echo "Waiting for shutdown for 5 seconds ..."
 sleep 5
 ps aux
-echo "Sleeping 5 seconds ..."
-sleep 5
-ps aux
-echo "Sleeping 5 seconds ..."
-sleep 5
-ps aux
-echo "Sleeping 5 seconds ..."
+echo "Waiting for shutdown for 5 seconds ..."
 sleep 5
 ps aux
 echo "Killing main process ..."
-killall python
+killall python 2>/dev/null
