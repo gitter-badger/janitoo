023f0292e9d0a55375d9d68c7bd4b830a6e7ce13
bibi21000 <bibi21000@gmail.com>
2016-01-01 15:34:39 +0100

    Update shutdown process

--- docker/auto.sh
+++ docker/auto.sh
@@ -4,4 +4,4 @@ cd /root/glances
 #git pull origin develop
 #You must loop here
 nice -n 19 python -m glances
-/root/halt.sh
+/root/shutdown.sh
--- docker/halt.sh
+++ docker/halt.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 echo "Halting ..."
-killall supervisord
-echo "Waiting for shutdown for 10 seconds ..."
-sleep 10
-ps aux
-echo "Waiting for shutdown for 10 seconds ..."
-sleep 10
-ps aux
-echo "Waiting for shutdown for 5 seconds ..."
-sleep 5
-ps aux
-echo "Waiting for shutdown for 5 seconds ..."
-sleep 5
-ps aux
-echo "Killing main process ..."
 killall python 2>/dev/null
