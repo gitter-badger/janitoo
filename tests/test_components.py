4e2272beef22a4e876ee65a42ab49b84d96fac5d
bibi21000 <bibi21000@gmail.com>
2016-01-31 16:57:31 +0100

    Some fixes

--- tests/test_components.py
+++ tests/test_components.py
@@ -46,9 +46,6 @@ from janitoo.runner import jnt_parse_args
 from janitoo.options import JNTOptions
 from janitoo.utils import HADD, HADD_SEP, CADD, json_dumps, json_loads
 
-from janitoo_raspberry_1wire.bus_1wire import OnewireBus
-import janitoo_raspberry_1wire.components as components
-
 ##############################################################
 #Check that we are in sync with the official command classes
 #Must be implemented for non-regression
