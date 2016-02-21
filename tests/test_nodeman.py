40d3f671e35b6dd1e7834168b41067b4a96301b8
bibi21000 <bibi21000@gmail.com>
2016-02-21 21:11:04 +0100

    Important update on server reload and way of running. Add and update tests

--- tests/test_nodeman.py
+++ tests/test_nodeman.py
@@ -155,7 +155,7 @@ class TestNodeManagerState(TestJanitoo):
         self.assertEqual(node_state.state, 'OFFLINE')
 
     def test_111_node_start_wait_random_stop(self):
-        self.onlyCITest()
+        self.onlyCircleTest()
         with mock.patch('sys.argv', [self.prog, 'start', '--conf_file=tests/data/test_nodeman.conf']):
             options = vars(jnt_parse_args())
             options = JNTOptions(options)
@@ -194,7 +194,7 @@ class TestNodeManagerState(TestJanitoo):
             time.sleep(1)
 
     def test_112_node_start_wait_random_stop_more(self):
-        self.onlyCITest()
+        self.onlyCircleTest()
         with mock.patch('sys.argv', [self.prog, 'start', '--conf_file=tests/data/test_nodeman.conf']):
             options = vars(jnt_parse_args())
             options = JNTOptions(options)
