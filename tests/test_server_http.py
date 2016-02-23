ce19d3129cd1ec4b64edbc64556391d8d11f4e87
bibi21000 <bibi21000@gmail.com>
2016-02-24 00:55:01 +0100

    Reenable test for server hhtp and remote on travis

--- tests/test_server_http.py
+++ tests/test_server_http.py
@@ -51,10 +51,10 @@ class TestHttpSerser(JNTTServer, JNTTServerCommon):
     server_conf = "tests/data/test_server_http.conf"
     hadds = [HADD%(1118,0), HADD%(1118,1)]
 
-    def test_040_server_start_no_error_in_log(self):
-        #The job exceeded the maxmimum time limit for jobs, and has been terminated on Travis
-        self.skipTravisTest()
-        JNTTServerCommon.test_040_server_start_no_error_in_log(self)
+    #~ def test_040_server_start_no_error_in_log(self):
+        #~ #The job exceeded the maxmimum time limit for jobs, and has been terminated on Travis
+        #~ self.skipTravisTest()
+        #~ JNTTServerCommon.test_040_server_start_no_error_in_log(self)
 
     def test_100_node_config(self):
         self.start()
