17b85c91576dcbd6337e5e39309da64ec646d8a9
bibi21000 <bibi21000@gmail.com>
2016-04-18 15:33:32 +0200

    Update tests for docker

--- tests/test_docker.py
+++ tests/test_docker.py
@@ -51,3 +51,98 @@ class TestFakeSerser(JNTTDockerServer, JNTTDockerServerCommon):
     server_class = JNTServer
     server_conf = "tests/data/test_threads_hourly.conf"
     hadds = [HADD%(1118,0), HADD%(1118,1)]
+
+
+class TestNetworkState(JNTTBase):
+    """Test the network state machine
+    """
+
+    prog = 'start.py'
+
+    add_ctrl = 111
+
+    def test_100_network_state_primary(self):
+        logging.config.fileConfig("tests/data/test_runner_conf_complete.conf")
+        with mock.patch('sys.argv', [self.prog, 'start', '--conf_file=tests/data/test_runner_conf_complete.conf']):
+            options = vars(jnt_parse_args())
+        stopevent = threading.Event()
+        net_state = JNTNetwork(stopevent, JNTOptions(options), is_primary=True, is_secondary=False, do_heartbeat_dispatch=True)
+        print net_state.state
+        hadds = { 0 : HADD%(self.add_ctrl,0),
+                     }
+        net_state.boot(hadds)
+        i = 0
+        while net_state.state != 'STARTED' and i<150:
+            i += 1
+            print net_state.state
+            time.sleep(1)
+        self.assertEqual(net_state.state, 'STARTED')
+        net_state.stop()
+        i = 0
+        while net_state.state != 'STOPPED' and i<150:
+            i += 1
+            print net_state.state
+            time.sleep(1)
+        self.assertEqual(net_state.state, 'STOPPED')
+
+    def test_110_network_state_secondary(self):
+        logging.config.fileConfig("tests/data/test_runner_conf_complete.conf")
+        with mock.patch('sys.argv', [self.prog, 'start', '--conf_file=tests/data/test_runner_conf_complete.conf']):
+            options = vars(jnt_parse_args())
+        stopevent = threading.Event()
+        net_state = JNTNetwork(stopevent, JNTOptions(options), is_primary=False, is_secondary=True, do_heartbeat_dispatch=False, resolv_timeout=10)
+        print net_state.state
+        hadds = { 0 : HADD%(self.add_ctrl,0),
+                     }
+        net_state.boot(hadds)
+        i = 0
+        while net_state.state != 'STARTED' and i<150:
+            i += 1
+            print net_state.state
+            time.sleep(1)
+        self.assertEqual(net_state.state, 'STARTED')
+        net_state.stop()
+        i = 0
+        while net_state.state != 'STOPPED' and i<150:
+            i += 1
+            print net_state.state
+            time.sleep(1)
+        self.assertEqual(net_state.state, 'STOPPED')
+
+    def test_120_network_state_secondary_fail(self):
+        logging.config.fileConfig("tests/data/test_runner_conf_complete.conf")
+        with mock.patch('sys.argv', [self.prog, 'start', '--conf_file=tests/data/test_runner_conf_complete.conf']):
+            options = vars(jnt_parse_args())
+        stopevent = threading.Event()
+        net_state = JNTNetwork(stopevent, JNTOptions(options), is_primary=False, is_secondary=True, do_heartbeat_dispatch=False, resolv_timeout=10)
+        print net_state.state
+        hadds = { 0 : HADD%(self.add_ctrl,0),
+                     }
+        net_state.boot(hadds)
+        i = 0
+        while net_state.state != 'STARTED' and i<150:
+            i += 1
+            print net_state.state
+            time.sleep(1)
+        self.assertEqual(net_state.state, 'STARTED')
+        net_state.fsm_network_fail()
+        print net_state.state
+        i = 0
+        while net_state.state != 'STARTED' and i<150:
+            i += 1
+            print net_state.state
+            time.sleep(1)
+        net_state.fsm_network_recover()
+        print net_state.state
+        i = 0
+        while net_state.state != 'STARTED' and i<150:
+            i += 1
+            print net_state.state
+            time.sleep(1)
+        net_state.stop()
+        i = 0
+        while net_state.state != 'STOPPED' and i<150:
+            i += 1
+            print net_state.state
+            time.sleep(1)
+        self.assertEqual(net_state.state, 'STOPPED')
